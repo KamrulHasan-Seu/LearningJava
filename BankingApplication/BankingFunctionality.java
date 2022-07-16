77d50604e8e0229f34a9ab9c417dc45299a41b3b
KamrulHasan-Seu <nirobrasseen@gmail.com>
2022-07-16 13:44:53 +0600

    Basic Banking operation 1.0.0

---
+++ BankingApplication/BankingFunctionality.java
@@ -0,0 +1,7 @@
+package BankingApplication;
+
+public  class BankingFunctionality {
+    public static void openAccount(){
+
+    }
+}
---
+++ BankingApplication/Menu.java
@@ -0,0 +1,14 @@
+package BankingApplication;
+
+public class Menu {
+    public static void showMenu(){
+        System.out.println("\n");
+        System.out.println("1. Open Account");
+        System.out.println("2. Show Account Information");
+        System.out.println("3. Deposit amount");
+        System.out.println("4. WithDraw Amount");
+        System.out.println("5. Show All Account Amount");
+        System.out.println("6. Exit");
+        System.out.println("\n");
+    }
+}
